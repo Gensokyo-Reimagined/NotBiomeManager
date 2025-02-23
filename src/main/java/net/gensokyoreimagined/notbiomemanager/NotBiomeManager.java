2a66562dce5c2c4a58fef5c7c2c0915668a8856b
kidofcubes <kidofcubes@gmail.com>
2025-02-23 17:48:28 +0800

    mostlyworksmethinks

--- src/main/java/net/gensokyoreimagined/notbiomemanager/NotBiomeManager.java
+++ src/main/java/net/gensokyoreimagined/notbiomemanager/NotBiomeManager.java
@@ -1,19 +1,26 @@
 package net.gensokyoreimagined.notbiomemanager;
 
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import it.unimi.dsi.fastutil.Pair;
+import net.minecraft.commands.arguments.ParticleArgument;
 import net.minecraft.core.*;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.biome.BiomeGenerationSettings;
-import net.minecraft.world.level.biome.BiomeSpecialEffects;
-import net.minecraft.world.level.biome.MobSpawnSettings;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.level.biome.*;
+import org.apache.commons.io.FileUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.Color;
 import org.bukkit.NamespacedKey;
+import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.configurate.CommentedConfigurationNode;
 import org.spongepowered.configurate.ConfigurateException;
 import org.spongepowered.configurate.ConfigurationNode;
@@ -27,8 +34,8 @@ import java.io.File;
 import java.lang.reflect.Field;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
+import java.util.function.Consumer;
 import java.util.logging.Logger;
 
 public class NotBiomeManager extends JavaPlugin {
@@ -74,44 +81,30 @@ public class NotBiomeManager extends JavaPlugin {
         Path biomesFolder = Paths.get(getDataFolder().getPath(),"biomes");
         biomesFolder.toFile().mkdirs();
 
-        for(File file: biomesFolder.toFile().listFiles()){
+        List<Pair<NamespacedKey, Biome>> toAddBiomes = new ArrayList<>();
+        FileUtils.iterateFiles(biomesFolder.toFile(), new String[]{"yml","yaml"},true).forEachRemaining(file -> {
             //if(FilenameUtils.getExtension(file.getPath()).equalsIgnoreCase());
+            System.out.println("Loading biome file "+file.getName());
             var loader = YamlConfigurationLoader.builder()
                     .file(file)
                     .defaultOptions(configurationOptions)
                     .build();
             try{
                 CommentedConfigurationNode node = loader.load();
-
+                toAddBiomes.add(loadBiomeConfig(node));
             }catch(ConfigurateException e){
                 e.printStackTrace();
-                continue;
             }
+        });
 
+        for(var pair : toAddBiomes){
+            System.out.println("Registering biome "+pair.key());
+            try{
+                registerBiome(pair.key(),pair.value());
+            }catch(Exception e){
+                e.printStackTrace();
+            }
         }
-
-
-        NamespacedKey namespacedKey = new NamespacedKey("minecraft","plains");
-
-
-        Biome biome = new Biome.BiomeBuilder()
-                .temperature(0.0f)
-                .downfall(0.0f)
-                .specialEffects(
-                        new BiomeSpecialEffects.Builder()
-                                .fogColor(Color.fromARGB(0,0,0,255).asRGB())
-                                .waterColor(Color.fromARGB(0,0,255,0).asRGB())
-                                .waterFogColor(Color.fromARGB(0,0,255,255).asRGB())
-                                .skyColor(Color.fromARGB(0,255,0,255).asRGB())
-                                .build()
-                )
-                .mobSpawnSettings(MobSpawnSettings.EMPTY)
-                .generationSettings(BiomeGenerationSettings.EMPTY)
-                .build();
-        registerBiome(namespacedKey,biome);
-
-
-
 
     }
 
@@ -120,36 +113,89 @@ public class NotBiomeManager extends JavaPlugin {
         return Color.fromRGB(Integer.parseInt(splitted[0]),Integer.parseInt(splitted[1]),Integer.parseInt(splitted[2]));
     }
 
-    public static String toRgbString(Color color){
-        return color.getRed()+"-"+color.getGreen()+"-"+color.getBlue();
+    Holder<SoundEvent> getSoundFromKey(String key){
+        return getSoundFromKey(NamespacedKey.fromString(key.toLowerCase()));
     }
 
-    String getOrDefault(ConfigurationNode node, String defaultValue){
-        var value = node.getString();
-        if(value == null){
-            value = defaultValue;
+    @Nullable
+    Holder<SoundEvent> getSoundFromKey(NamespacedKey key){
+        return BuiltInRegistries.SOUND_EVENT.get(ResourceLocation.fromNamespaceAndPath(key.namespace(),key.value())).orElse(null);
+    }
+
+
+    void compute(ConfigurationNode node,Consumer<ConfigurationNode> consumer){
+        if(!node.empty()){
+            consumer.accept(node);
         }
-        return value;
     }
 
     public Pair<NamespacedKey,Biome> loadBiomeConfig(ConfigurationNode node){
-        NamespacedKey key = NamespacedKey.fromString(node.node("Key").getString());
+        NamespacedKey key = NamespacedKey.fromString(node.node("Key").getString().toLowerCase());
         boolean custom = node.node("Custom").getBoolean();
 
         Registry<Biome> biomes = MinecraftServer.getServer().registryAccess().lookup(Registries.BIOME).orElseThrow();
-        Biome base = (biomes.get(ResourceLocation.fromNamespaceAndPath("minecraft", node.node("Base").getString())).get()).value();
+        Biome base = (biomes.get(ResourceLocation.fromNamespaceAndPath("minecraft", node.node("Base").getString().toLowerCase())).get()).value();
         var biomeBuilder = new Biome.BiomeBuilder();
-        var specialEffectsBuilder = new BiomeSpecialEffects.Builder();
+
+        biomeBuilder.hasPrecipitation(base.climateSettings.hasPrecipitation());
+        biomeBuilder.temperature(base.climateSettings.temperature());
+        biomeBuilder.temperatureAdjustment(base.climateSettings.temperatureModifier());
+        biomeBuilder.downfall(base.climateSettings.downfall());
+
+        biomeBuilder.mobSpawnSettings(base.getMobSettings());
+        biomeBuilder.generationSettings(base.getGenerationSettings());
+
+        var specialEffectsBuilder = SpecialEffectsBuilder.getSpecialEffects(base);
         var specialEffectsNode = node.node("Special_Effects");
 
-        specialEffectsBuilder.fogColor((fromRgbString(getOrDefault(specialEffectsNode.node("Fog_Color"),toRgbString(Color.fromRGB(base.getFogColor()))))).asRGB());
-        specialEffectsBuilder.waterColor((fromRgbString(getOrDefault(specialEffectsNode.node("Water_Color"),toRgbString(Color.fromRGB(base.getFogColor()))))).asRGB());
-        specialEffectsBuilder.waterFogColor((fromRgbString(getOrDefault(specialEffectsNode.node("Water_Fog_Color"),toRgbString(Color.fromRGB(base.getFogColor()))))).asRGB());
-        specialEffectsBuilder.skyColor((fromRgbString(getOrDefault(specialEffectsNode.node("Sky_Color"),toRgbString(Color.fromRGB(base.getFogColor()))))).asRGB());
 
-        if(specialEffectsNode.node("Grass_Modified").getString() !=null){
-            specialEffectsBuilder.grassColorModifier(BiomeSpecialEffects.GrassColorModifier.valueOf(specialEffectsNode.node("Grass_Modified").getString()));
-        }
+        compute(specialEffectsNode.node("Fog_Color"), x -> specialEffectsBuilder.fogColor(fromRgbString(x.getString()).asRGB()));
+        compute(specialEffectsNode.node("Water_Color"), x -> specialEffectsBuilder.waterColor(fromRgbString(x.getString()).asRGB()));
+        compute(specialEffectsNode.node("Water_Fog_Color"), x -> specialEffectsBuilder.waterFogColor(fromRgbString(x.getString()).asRGB()));
+        compute(specialEffectsNode.node("Sky_Color"), x -> specialEffectsBuilder.skyColor(fromRgbString(x.getString()).asRGB()));
+//        specialEffectsBuilder.fogColor((fromRgbString(getOrDefault(specialEffectsNode.node("Fog_Color"),toRgbString(Color.fromRGB(base.getFogColor()))))).asRGB());
+//        specialEffectsBuilder.waterColor((fromRgbString(getOrDefault(specialEffectsNode.node("Water_Color"),toRgbString(Color.fromRGB(base.getFogColor()))))).asRGB());
+//        specialEffectsBuilder.waterFogColor((fromRgbString(getOrDefault(specialEffectsNode.node("Water_Fog_Color"),toRgbString(Color.fromRGB(base.getFogColor()))))).asRGB());
+//        specialEffectsBuilder.skyColor((fromRgbString(getOrDefault(specialEffectsNode.node("Sky_Color"),toRgbString(Color.fromRGB(base.getFogColor()))))).asRGB());
+
+        compute(specialEffectsNode.node("Grass_Modifier"), x -> specialEffectsBuilder.grassColorModifier(BiomeSpecialEffects.GrassColorModifier.valueOf(x.getString())));
+
+        var ambientParticleNode = specialEffectsNode.node("Particle");
+        compute(ambientParticleNode.node("Type"), x -> {
+            RegistryAccess access = ((CraftServer) Bukkit.getServer()).getServer().registryAccess();
+            ParticleOptions nmsParticle = null;
+            try{
+                nmsParticle = ParticleArgument.readParticle(new StringReader(x.getString()), access);
+            }catch(CommandSyntaxException e){
+                throw new RuntimeException(e);
+            }
+            specialEffectsBuilder.ambientParticleParticleOptions = Optional.of(nmsParticle);
+        });
+        compute(ambientParticleNode.node("Density"), x -> specialEffectsBuilder.ambientParticleProbability = Optional.of(x.getFloat()));
+
+        var ambientMoodNode = specialEffectsNode.node("Cave_Sound");
+        compute(ambientMoodNode.node("Sound"), x -> specialEffectsBuilder.ambientMoodSoundEvent = Optional.ofNullable(getSoundFromKey(x.getString())));
+        compute(ambientMoodNode.node("Tick_Delay"), x -> specialEffectsBuilder.ambientMoodTickDelay = Optional.of(x.getInt()));
+        compute(ambientMoodNode.node("Search_Distance"), x -> specialEffectsBuilder.ambientMoodBlockSearchExtent = Optional.of(x.getInt()));
+        compute(ambientMoodNode.node("Sound_Offset"), x -> specialEffectsBuilder.ambientMoodSoundPositionOffset = Optional.of(x.getDouble()));
+
+
+        var ambientAdditionsNode = specialEffectsNode.node("Random_Sound");
+        compute(ambientAdditionsNode.node("Sound"), x -> specialEffectsBuilder.ambientAdditionsSoundEvent = Optional.ofNullable(getSoundFromKey(x.getString())));
+        compute(ambientAdditionsNode.node("Tick_Chance"), x -> specialEffectsBuilder.ambientAdditionsTickChance = Optional.of(x.getDouble()));
+
+
+
+        compute(specialEffectsNode.node("Music"), x -> {
+            if(x.isList()){
+                throw new RuntimeException("Unsupported list syntax for Music");
+            }else{
+                compute(x.node("Sound"), y -> specialEffectsBuilder.singleBackgroundMusicSoundEvent = Optional.ofNullable(getSoundFromKey(y.getString())));
+                compute(x.node("Min_Delay"), y -> specialEffectsBuilder.singleBackgroundMusicMinDelay = Optional.of(x.getInt()));
+                compute(x.node("Max_Delay"), y -> specialEffectsBuilder.singleBackgroundMusicMaxDelay = Optional.of(x.getInt()));
+                compute(x.node("Override_Previous_Music"), y -> specialEffectsBuilder.singleBackgroundMusicReplaceCurrentMusic = Optional.of(y.getBoolean()));
+            }
+        });
 
         biomeBuilder.specialEffects(specialEffectsBuilder.build());
 
---
+++ src/main/java/net/gensokyoreimagined/notbiomemanager/SpecialEffectsBuilder.java
@@ -0,0 +1,197 @@
+package net.gensokyoreimagined.notbiomemanager;
+
+import net.minecraft.core.Holder;
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.sounds.Music;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.util.random.SimpleWeightedRandomList;
+import net.minecraft.world.level.biome.*;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.Nullable;
+import java.lang.reflect.Field;
+import java.util.Optional;
+
+public class SpecialEffectsBuilder extends BiomeSpecialEffects.Builder {
+    private SpecialEffectsBuilder(){}
+    public static SpecialEffectsBuilder getSpecialEffects(Biome biome) {
+        BiomeSpecialEffects effects = biome.getSpecialEffects();
+
+        SpecialEffectsBuilder builder = new SpecialEffectsBuilder();
+        builder.fogColor(effects.getFogColor())
+                .waterColor(effects.getWaterColor())
+                .waterFogColor(effects.getWaterFogColor())
+                .skyColor(effects.getSkyColor())
+                .grassColorModifier(effects.getGrassColorModifier())
+        ;
+
+        effects.getGrassColorOverride().ifPresent(builder::grassColorOverride);
+        effects.getFoliageColorOverride().ifPresent(builder::foliageColorOverride);
+        effects.getAmbientLoopSoundEvent().ifPresent(builder::ambientLoopSound);
+
+        if (effects.getAmbientParticleSettings().isPresent()) {
+            AmbientParticleSettings particle = effects.getAmbientParticleSettings().get();
+
+            builder.ambientParticle(particle);
+        }
+
+        if (effects.getAmbientMoodSettings().isPresent()) {
+            AmbientMoodSettings settings = effects.getAmbientMoodSettings().get();
+
+            builder.ambientMoodSound(settings);
+        }
+
+        if (effects.getAmbientAdditionsSettings().isPresent()) {
+            AmbientAdditionsSettings settings = effects.getAmbientAdditionsSettings().get();
+
+            builder.ambientAdditionsSound(settings);
+        }
+
+        if (effects.getBackgroundMusic().isPresent()) {
+            builder.backgroundMusic(effects.getBackgroundMusic().get());
+        }
+
+        return builder;
+    }
+
+
+    private Optional<SimpleWeightedRandomList<Music>> backgroundMusic = Optional.empty();
+
+    public BiomeSpecialEffects.Builder backgroundMusic(@Nullable Music backgroundMusic) {
+        super.backgroundMusic(backgroundMusic);
+        if (backgroundMusic == null) {
+            this.backgroundMusic = Optional.empty();
+            return this;
+        } else {
+            this.backgroundMusic = Optional.of(SimpleWeightedRandomList.single(backgroundMusic));
+            return this;
+        }
+    }
+
+    public BiomeSpecialEffects.Builder backgroundMusic(SimpleWeightedRandomList<Music> backgroundMusic) {
+        super.backgroundMusic(backgroundMusic);
+        this.backgroundMusic = Optional.of(backgroundMusic);
+        return this;
+    }
+
+
+    public Optional<Holder<SoundEvent>>    ambientMoodSoundEvent = Optional.empty();
+    public Optional<Integer>               ambientMoodTickDelay = Optional.empty();
+    public Optional<Integer>               ambientMoodBlockSearchExtent = Optional.empty();
+    public Optional<Double>                ambientMoodSoundPositionOffset = Optional.empty();
+
+    public Optional<ParticleOptions>       ambientParticleParticleOptions = Optional.empty();
+    public Optional<Float>                 ambientParticleProbability = Optional.empty();
+
+    public Optional<Holder<SoundEvent>>    ambientAdditionsSoundEvent = Optional.empty();
+    public Optional<Double>                ambientAdditionsTickChance = Optional.empty();
+
+    public Optional<Holder<SoundEvent>>    singleBackgroundMusicSoundEvent = Optional.empty();
+    public Optional<Integer>               singleBackgroundMusicMinDelay = Optional.empty();
+    public Optional<Integer>               singleBackgroundMusicMaxDelay = Optional.empty();
+    public Optional<Boolean>               singleBackgroundMusicReplaceCurrentMusic = Optional.empty();
+
+    private static final Field probabilityField;
+
+    static{
+        try{
+            probabilityField = AmbientParticleSettings.class.getDeclaredField("probability");
+            probabilityField.setAccessible(true);
+        }catch(NoSuchFieldException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public BiomeSpecialEffects.@NotNull Builder ambientParticle(AmbientParticleSettings ambientParticle) {
+        this.ambientParticleParticleOptions = Optional.of(ambientParticle.getOptions());
+        try{
+            this.ambientParticleProbability = Optional.of((Float)probabilityField.get(ambientParticle));
+        }catch(IllegalAccessException e){
+            throw new RuntimeException(e);
+        }
+        return this;
+    }
+
+
+    @Override
+    public BiomeSpecialEffects.@NotNull Builder ambientMoodSound(AmbientMoodSettings ambientMoodSettings) {
+        this.ambientMoodSoundEvent = Optional.of(ambientMoodSettings.getSoundEvent());
+        this.ambientMoodTickDelay = Optional.of(ambientMoodSettings.getTickDelay());
+        this.ambientMoodBlockSearchExtent = Optional.of(ambientMoodSettings.getBlockSearchExtent());
+        this.ambientMoodSoundPositionOffset = Optional.of(ambientMoodSettings.getSoundPositionOffset());
+        return this;
+    }
+
+    @Override
+    public BiomeSpecialEffects.@NotNull Builder ambientAdditionsSound(AmbientAdditionsSettings ambientAdditionsSettings) {
+        this.ambientAdditionsSoundEvent = Optional.of(ambientAdditionsSettings.getSoundEvent());
+        this.ambientAdditionsTickChance = Optional.of(ambientAdditionsSettings.getTickChance());
+        return this;
+    }
+
+
+
+    @Override
+    public @NotNull BiomeSpecialEffects build(){
+
+        if(
+                ambientParticleParticleOptions.isPresent() &&
+                ambientParticleProbability.isPresent()
+        ){
+            this.ambientParticle(new AmbientParticleSettings(ambientParticleParticleOptions.get(),ambientParticleProbability.get()));
+        }
+
+        if(
+                ambientMoodSoundEvent.isPresent() &&
+                ambientMoodTickDelay.isPresent() &&
+                ambientMoodBlockSearchExtent.isPresent() &&
+                ambientMoodSoundPositionOffset.isPresent()
+        ){
+            if(ambientMoodSoundEvent.get()!=null){
+                this.ambientMoodSound(new AmbientMoodSettings(ambientMoodSoundEvent.get(),ambientMoodTickDelay.get(),ambientMoodBlockSearchExtent.get(),ambientMoodSoundPositionOffset.get()));
+            }
+        }
+
+        if(
+                ambientAdditionsSoundEvent.isPresent() &&
+                ambientAdditionsTickChance.isPresent()
+        ){
+            this.ambientAdditionsSound(new AmbientAdditionsSettings(ambientAdditionsSoundEvent.get(),ambientAdditionsTickChance.get()));
+        }
+
+        if(
+                singleBackgroundMusicSoundEvent.isPresent() ||
+                singleBackgroundMusicMinDelay.isPresent() ||
+                singleBackgroundMusicMaxDelay.isPresent() ||
+                singleBackgroundMusicReplaceCurrentMusic.isPresent()
+        ){
+            if(
+
+                    singleBackgroundMusicSoundEvent.isPresent() &&
+                    singleBackgroundMusicMinDelay.isPresent() &&
+                    singleBackgroundMusicMaxDelay.isPresent() &&
+                    singleBackgroundMusicReplaceCurrentMusic.isPresent()
+            ){
+                if(singleBackgroundMusicSoundEvent.get()!=null){
+                    this.backgroundMusic(new Music(singleBackgroundMusicSoundEvent.get(),singleBackgroundMusicMinDelay.get(),singleBackgroundMusicMaxDelay.get(),singleBackgroundMusicReplaceCurrentMusic.get()));
+                }else{
+                    this.backgroundMusic((Music)null);
+                }
+            }else if(this.backgroundMusic.isEmpty() || this.backgroundMusic.get().unwrap().size() != 1){
+                //System.out.println("things are "+singleBackgroundMusicSoundEvent+" and "+singleBackgroundMusicMinDelay+" and "+singleBackgroundMusicMaxDelay+" and "+singleBackgroundMusicReplaceCurrentMusic);
+                System.out.println("Base background music has invalid length for single overriding syntax / not enough valid configuration values provided for a single track");
+            }else{
+                var original = this.backgroundMusic.get().unwrap().getFirst().data();
+                this.backgroundMusic(new Music(
+                        singleBackgroundMusicSoundEvent.orElse(original.getEvent()),
+                        singleBackgroundMusicMinDelay.orElse(original.getMinDelay()),
+                        singleBackgroundMusicMaxDelay.orElse(original.getMaxDelay()),
+                        singleBackgroundMusicReplaceCurrentMusic.orElse(original.replaceCurrentMusic())
+                ));
+            }
+        }
+
+        return super.build();
+    }
+}
